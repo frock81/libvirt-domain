5c7620fa0ef74b161dab4007257f7744d1c9b86c
Fabio Rocha <frock81@yahoo.com.br>
2020-09-06 21:29:10 -0300

    Test boilerplate

--- module_utils/test_xml_filter.py
+++ module_utils/test_xml_filter.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python3
+
 import unittest
+from lxml import etree
+from xml_filter import XmlFilter
 
 class TestXmlFilter(unittest.TestCase):
-    FILTER = {
+    FILTER_SPEC = {
         'domain': {
-            '__attributes__': ['type']
+            '__attributes__': ['type'],
             'name': {},
             'uuid': {},
             'title': {'required': False},
@@ -19,3 +23,13 @@ class TestXmlFilter(unittest.TestCase):
             }
         }
     }
+
+    def test_domain(self):
+        xml_file = 'domain.xml'
+        with open(xml_file, 'r') as file:
+            xml_string = file.read()
+        xml_filter = XmlFilter(filter_spec=self.FILTER_SPEC,
+            input_xml=xml_string)
+
+if __name__ == '__main__':
+    unittest.main()
