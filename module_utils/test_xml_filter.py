641b41fcbf4497b1759869a7eacd45f960875290
Fabio <frock81@yahoo.com.br>
2020-09-07 18:42:16 -0300

    Test and implementation for name

--- module_utils/test_xml_filter.py
+++ module_utils/test_xml_filter.py
@@ -33,10 +33,29 @@ class TestXmlFilter(unittest.TestCase):
             input_xml=xml_string)
         cls.xml_tree = etree.fromstring(xml_filter.output_xml)
 
+    @classmethod
+    def tearDownClass(cls):
+        print(etree.tostring(cls.xml_tree, pretty_print=True))
+
     def test_domain(self):
         self.assertEqual(self.xml_tree.tag, 'domain')
         self.assertEqual(self.xml_tree.get('type'), 'kvm')
         self.assertFalse(self.xml_tree.get('id'))
+
+    def test_name(self):
+        node_name = 'name'
+        query = '/domain/{0}'.format(node_name)
+        result = self.xml_tree.xpath(query)
+        self.assertEqual(len(result), 1)
+        node = result[0]
+        self.assertEqual(node.tag, node_name)
+        self.assertEqual(node.text, 'vm-algol')
+
+    def test_features_out(self):
+        node_name = 'features'
+        query = '/domain/{0}'.format(node_name)
+        result = self.xml_tree.xpath(query)
+        self.assertFalse(result)
 
 if __name__ == '__main__':
     unittest.main()
--- module_utils/xml_filter.py
+++ module_utils/xml_filter.py
@@ -25,22 +25,28 @@ class XmlFilter:
             xpath_query_result = self.__input_xml_tree.xpath(xpath)
             self.__process_from_input_nodes(result_list=xpath_query_result,
                 parent_output_node=parent_output_node,
-                filter_spec=filter_spec[node_name])
+                filter_spec=filter_spec[node_name],
+                xpath=xpath)
 
     def __process_from_input_nodes(
         self,
         result_list,
         parent_output_node,
-        filter_spec
+        filter_spec,
+        xpath=''
     ):
         for input_node in result_list:
             output_node = self.__create_output_node(node_name=input_node.tag,
                 parent_output_node=parent_output_node)
             self.__process_attributes(input_node=input_node,
                 output_node=output_node, filter_spec=filter_spec)
+            self.__process_text(input_node=input_node, output_node=output_node)
+            self.__process_filter(filter_spec=filter_spec,
+                parent_output_node=output_node,
+                parent_xpath=xpath)
 
     def __create_output_node(self, node_name, parent_output_node):
-        if not parent_output_node:
+        if parent_output_node is None:
             self.__output_xml_tree = etree.Element(node_name)
             return self.__output_xml_tree
         return etree.SubElement(parent_output_node, node_name)
@@ -56,6 +62,10 @@ class XmlFilter:
             attribute_value = input_node.get(attribute_name)
             if attribute_value:
                 output_node.set(attribute_name, attribute_value)
+
+    def __process_text(self, input_node, output_node):
+        if input_node.text:
+            output_node.text = input_node.text
 
     @property
     def output_xml(self):
