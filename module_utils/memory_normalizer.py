d22204534e6169f5f9d4d06f75c03a5a6b2178f8
Fabio Rocha <frock81@yahoo.com.br>
2020-09-19 18:54:07 -0300

    Minor refactor
    Create a generic function to retrieve memory.

--- module_utils/memory_normalizer.py
+++ module_utils/memory_normalizer.py
@@ -8,8 +8,16 @@ class MemoryNormalizer():
         self._normalized = intermediate
 
     def _get_memory_element(self, intermediate):
-        return next(filter(lambda child : child['element_name'] ==
-            'memory', intermediate['children']))
+        return self._get_element_from_list(element_list=
+            intermediate['children'], element_name='memory')
+
+    def _get_element_from_list(self, element_list, element_name):
+        try:
+            return next(filter(lambda element : element['element_name'] ==
+                element_name, element_list))
+        except StopIteration as e:
+            return None
+        return None
 
     def _get_attribute_from_element(self, element, attribute_name):
         if 'attributes' in element:
